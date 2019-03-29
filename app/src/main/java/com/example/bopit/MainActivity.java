57599d35aa40c93ddf7a2fa2d03f26101ac741f5
IsaacEarnest <earnestia@hendrix.edu>
2019-03-28 20:41:57 -0500

    MainActivity: Working buttons and difficulty selection

--- app/src/main/java/com/example/bopit/MainActivity.java
+++ app/src/main/java/com/example/bopit/MainActivity.java
@@ -7,21 +7,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
     private String difficulty;
     private Button start;
     private Button setDifficulty;
+    private Button diffSelectionButton;//rename so it's not redundant
     private AlertDialog difficultySelection;
+    private View mView;
+    private RadioButton easy, medium, hard;
+    private RadioGroup difficulties;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         difficulty="Easy";
-        initUI();
         initDifficultySelect();
+        initUI();
         initOnClickListeners();
 
 
@@ -35,14 +42,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initUI(){
-        Log.d(TAG,"initUI: initiali");
+        Log.d(TAG,"initUI: initializing UI components");
         start = findViewById(R.id.start);
         setDifficulty = findViewById(R.id.difficulty);
+        difficulties = mView.findViewById(R.id.difficulties);
+        easy = mView.findViewById(R.id.easy);
+        medium = mView.findViewById(R.id.medium);
+        hard = mView.findViewById(R.id.hard);
+        diffSelectionButton = mView.findViewById(R.id.difficultySelectButton);
     }
     private void initOnClickListeners(){
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 openGameActivity();
             }
         });
@@ -51,13 +62,28 @@ public class MainActivity extends AppCompatActivity {
                 difficultySelection.show();
             }
         });
+        diffSelectionButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(easy.isChecked()){
+                    difficulty = "Easy";
+                }else if(medium.isChecked()){
+                    difficulty = "Medium";
+                }else if(hard.isChecked()){
+                    difficulty = "Hard";
+                }
+                difficultySelection.dismiss();
+                Toast.makeText(getApplicationContext(),"Difficulty set to "+difficulty+"!",Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"diffSelectionButton onClick: Changed difficulty to: "+ difficulty);
+            }
+        });
     }
 
     private void initDifficultySelect (){
-        final View mView = getLayoutInflater().inflate(R.layout.dialog_difficulty_selection, null);
+        mView = getLayoutInflater().inflate(R.layout.dialog_difficulty_selection, null);
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
         mBuilder.setView(mView);
          difficultySelection = mBuilder.create();
+
     }
 
 }
